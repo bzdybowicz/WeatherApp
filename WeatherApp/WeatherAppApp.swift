d1387721502e313388678ef41c5b32e7ff24ff5a
Bartłomiej Zdybowicz <bartlomiej.zdybowicz@gmail.com>
2023-10-25 20:41:40 +0200

    Project init

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Bartlomiej Zdybowicz on 25/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
